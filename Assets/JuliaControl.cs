1b8565a674e89d9b3e21eb0dd18a42097c82aaea
densy07 <laiqifang@gmail.com>
2019-02-17 15:43:11 +0800

    create

---
+++ Assets/JuliaControl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuliaControl : MonoBehaviour
+{
+    public float m_scale = 1;
+
+    private Vector2 m_downPos;
+    private Vector2 m_lastPos;
+    private bool m_hasMouseDown;
+
+    private Vector2 _screenP;
+
+    public Material m_mat;
+
+    public bool m_doUpdate = true;
+
+    public float _C;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        m_hasMouseDown = false;
+        _screenP = new Vector2 (Screen.width / 2, Screen.height / 2);
+        m_mat.SetVector ("_P", new Vector4 (0, 0, 0, 0));
+
+        var uvScale = Vector2.one;
+        if (Screen.width > Screen.height)
+        {
+            uvScale.x = Screen.width / (float) Screen.height;
+        }
+        else
+        {
+            uvScale.y = Screen.height / (float) Screen.width;
+        }
+
+        Debug.Log (uvScale);
+        m_mat.SetVector ("_uvScale", uvScale);
+    }
+
+    private void OnRenderImage (RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit (src, dest, m_mat);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (m_mat == null || m_doUpdate == false)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown (0))
+        {
+            m_downPos = Input.mousePosition;
+            m_lastPos = m_downPos;
+        }
+
+        if (Input.GetMouseButton (0))
+        {
+            Vector2 currentPos = Input.mousePosition;
+            _screenP += (currentPos - m_lastPos) * m_scale;
+            m_lastPos = currentPos;
+
+            var pos = _screenP;
+
+            var x = pos.x / Screen.width;
+            var y = pos.y / Screen.height;
+
+            x = (x - 0.5f) * 2;
+            y = (y - 0.5f) * 2;
+
+            m_mat.SetVector ("_P", new Vector4 (x, y, 0, 0));
+        }
+
+        var mouseDelta = Input.mouseScrollDelta;
+
+        if (mouseDelta.y > 0)
+        {
+            m_scale /= 1.1f;
+        }
+        else if (mouseDelta.y < 0)
+        {
+            m_scale *= 1.1f;
+        }
+
+        m_mat.SetFloat ("_S", m_scale);
+        m_mat.SetVector ("_C", new Vector4 (_C, _C));
+    }
+}
---
+++ Assets/JuliaDemo.shader
@@ -0,0 +1,85 @@
+﻿Shader "JuliaDemo"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _C("", Vector)=(0, 0, 0, 0)
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float2 _C;
+            float2 _P;
+            float _S;
+            float2 _uvScale;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+            
+            float Julia (float2 pos)
+            {
+                int count = 200;
+                pos = pos;
+                
+                float2 ret;
+                for(int i = 0; i < count; i++)
+                {
+                    ret.x =  pos.x * pos.x - pos.y * pos.y + _C.x;
+                    ret.y = pos.x * pos.y * 2 + _C.x;
+                    
+                    if (length(ret) > 2)
+                    {
+                        return ((i+1) / (float)count) / 2 + 0.5; 
+                    }
+                    
+                    pos = ret;
+                }
+                return 0;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                float2 pos = i.uv;
+                pos = ((pos * 2 - float2(1, 1)) * _S - _P) * _uvScale;;
+                
+                float c = Julia(pos);
+                fixed4 col;
+                
+                col = fixed4(1, 1, 1, 1) * c;
+                col.a = 1;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
